dcc7fbf4ad0eeb6fdcedf2d366d6562f7555306f
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2024-08-29 05:28:35 -0400

    Add function to get computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
-console.log("test");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    
+    if (0 === choice) {
+        return "rock";
+    }
+    else if (1 === choice) {
+        return "paper";
+    }
+    else if (2 === choice) {
+        return "scissors";
+    }
+    else {
+        return undefined;
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(getComputerChoice());
+}
